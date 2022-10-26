5af6a9f79de73295d0b72a4ee25be689db59c6b4
S. Holden Jones <jonessh@hawaii.edu>
2022-10-26 16:02:01 -0500

    working in script file b/c of wd issues

---
+++ scripts/ranas_explore.R
@@ -0,0 +1,51 @@
+
+# ranas_explore
+
+# loading data, quick looks
+
+library(tidyverse)
+
+ranas <- read_csv("data/ranas_10_26_22.csv")
+
+new <- ranas %>%
+  filter(Recap == "N")
+
+# simple ind caught for each site - just look at number new ind caught
+new %>%
+  group_by(Sitio) %>%
+  summarize(count = n_distinct(Numero)) %>%
+  ggplot(aes(Sitio, count)) +
+  geom_col()
+
+# how has number of ind changed over time?
+new %>%
+  group_by(Fecha) %>%
+  summarize(count = n_distinct(Numero)) %>%
+  ggplot(aes(Fecha, count)) +
+  geom_col()
+
+new_test <- new %>%
+  group_by(Fecha) %>%
+  mutate(new_ind = as.numeric(max(Numero)))
+
+# want a fig with overall number of ind increasing over time
+
+### working ###
+new_test %>%
+  ggplot(aes(x = factor(Fecha, level = c(
+    '01/09/22', '02/09/22', '03/09/22', '05/09/22', '06/09/22',
+    '07/09/22', '08/09/22', '09/09/22', '10/09/22', '12/09/22',
+    '13/09/22', '14/09/22', '15/09/22', '17/09/22', '19/09/22',
+    '03/10/22', '05/10/22', '07/10/22', '08/10/22', '10/10/22', 
+    '12/10/22')), 
+    y = new_ind)) +
+  geom_point() +
+  theme(axis.title.x=element_blank(),
+        axis.text.x=element_blank(),
+        axis.ticks.x=element_blank()) +
+  geom_smooth(method = "lm", se = FALSE)
+
+
+
+
+
